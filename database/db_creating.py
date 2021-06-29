6082cdbbb3bcc30b5e9bcbc477f20f890e9e8067
XenoProxy <ksenomorph6@gmail.com>
2021-06-29 15:38:20 +0300

    [*] user table

--- database/db_creating.py
+++ database/db_creating.py
@@ -18,7 +18,6 @@ def create_tables():
         tbl_users_create = """
             CREATE TABLE IF NOT EXISTS service_station.`tbl_user` (
             `user_id` BIGINT NULL AUTO_INCREMENT,
-            `user_name` VARCHAR(45) NULL,
             `user_username` VARCHAR(45) NULL,
             `user_password` VARCHAR(45) NULL,
             PRIMARY KEY (`user_id`))
