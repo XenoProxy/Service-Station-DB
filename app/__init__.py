e5acb775a151c1eb08cad276d4ecd4cea9903c04
XenoProxy <ksenomorph6@gmail.com>
2021-07-09 14:18:34 +0300

    [-] pymysql connection removed

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from config import Config
 
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
--- config.py
+++ config.py
@@ -1,18 +1,8 @@
 import os
-from database.connection import get_connection as conn
 
 
 class Config(object):
-    if conn():
-        SECRET_KEY = os.environ.get("SECRET_KEY") or "false key"
-        SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{}:{}1234@{}:{}/{}".format(
-                conn().user,
-                conn().password,
-                conn().host,
-                conn().port,
-                conn().db
-            )
-        SQLALCHEMY_TRACK_MODIFICATIONS = False
-        print("successful connection!")
-    else:
-        print("connection failed!")
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "false key"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:1234@localhost:3306/service_station"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    print("successful connection!")
