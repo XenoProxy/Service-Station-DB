e9000b588eabab6230fa48775783864462c3c583
XenoProxy <ksenomorph6@gmail.com>
2021-07-09 15:05:02 +0300

    [+] Clients table

--- database/models.py
+++ database/models.py
@@ -7,6 +7,28 @@ class Auto(db.Model):
     brand = db.Column(db.String(15), primary_key=True)
     model = db.Column(db.String(30), primary_key=True)
     make = db.Column(db.Date, primary_key=True)
+    owner = db.Column()
 
     def __repr__(self):
-        return "<VIN: {}>".format(self.vin)
+        info = "VIN:{}\nNumber:{}\nBrand:{}\nModel:{}\nMake:{}".format(
+            self.vin,
+            self.number,
+            self.brand,
+            self.model,
+            self.make,
+        )
+        return info
+
+
+class Clients(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(15),)
+    surname = db.Column(db.String(20),)
+
+    def __repr__(self):
+        info = "Id:{}\nName:{}\nSurname:{}\n".format(
+            self.id,
+            self.name,
+            self.surname,
+        )
+        return info
