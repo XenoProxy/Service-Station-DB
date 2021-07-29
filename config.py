668d1c18994afa391ab19fb059406c5bad41f656
XenoProxy <ksenomorph6@gmail.com>
2021-07-29 22:49:16 +0300

    [*] uft8mb4

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "false key"
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:1234@localhost:3306/service_station"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_NATIVE_UNICODE = "utf8mb4"
     print("successful connection!")
