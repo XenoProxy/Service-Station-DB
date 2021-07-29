c47d0bd92f40202d2ec191697cac6bb18c5e764f
XenoProxy <ksenomorph6@gmail.com>
2021-07-29 23:00:05 +0300

    [+] auto-model

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    SubmitField,
+    DateField,
+)
 from wtforms.validators import DataRequired
 
 
@@ -10,7 +16,16 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Sign In")
 
 
-class ClientInsertData(FlaskForm):
+class Clients(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     surname = StringField("Surname", validators=[DataRequired()])
+    submit = SubmitField("Add")
+
+
+class Auto(FlaskForm):
+    vin = StringField("Vin", validators=[DataRequired()])
+    number = StringField("Number", validators=[DataRequired()])
+    brand = StringField("Brand", validators=[DataRequired()])
+    model = StringField("Model", validators=[DataRequired()])
+    make = DateField("Make", validators=[DataRequired()])
     submit = SubmitField("Add")
